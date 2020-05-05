39125cd275d5662e8fbaa92d49aab02f045d62a5
pkunzipula <jcapra76@gmail.com>
2020-05-04 21:39:44 -0700

    Navigation Part I

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,27 +1,40 @@
 import React from "react";
 import { FlatList } from "react-native";
 import { ListItem } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
-function Directory(props) {
-  const renderDirectoryItem = ({ item }) => {
-    return (
-      <ListItem
-        title={item.name}
-        subtitle={item.description}
-        leftAvatar={{ source: require("../shared/images/react-lake.jpg") }}
-      />
-    );
+class Directory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Directory",
   };
 
-  const extractZeeKey = (item) => item.id.toString();
-
-  return (
-    <FlatList
-      data={props.campsites}
-      renderItem={renderDirectoryItem}
-      keyExtractor={extractZeeKey}
-    />
-  );
+  render() {
+    const { navigate } = this.props.navigation;
+    const renderDirectoryItem = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: require("./images/react-lake.jpg") }}
+          onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
+        />
+      );
+    };
+    return (
+      <FlatList
+        data={campsites}
+        renderItem={renderDirectoryItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    );
+  }
 }
 
 export default Directory;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,16 +1,54 @@
 import React, { Component } from "react";
+import { View, Platform } from "react-native";
+import { createStackNavigator } from "react-navigation";
 import Directory from "./DirectoryComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
 import { CAMPSITES } from "../shared/campsites";
+
+const DirectoryNavigator = createStackNavigator(
+  {
+    Directory: { screen: Directory },
+    CampsiteInfo: { screen: CampsiteInfo },
+  },
+  {
+    initialRouteName: "Directory",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      selectedCampsite: null,
     };
   }
+
+  onCampsiteSelect(campsiteId) {
+    this.setState({ selectedCampsite: campsiteId });
+  }
+
   render() {
-    return <Directory campsites={this.state.campsites} />;
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingTop:
+            Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+        }}
+      >
+        <DirectoryNavigator />
+      </View>
+    );
   }
 }
 
