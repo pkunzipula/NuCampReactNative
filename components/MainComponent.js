9d5a13680e63bc3231a591f38028c5b5bda45a21
pkunzipula <jcapra76@gmail.com>
2020-05-08 17:55:08 -0700

    Revert "Forms"
    This reverts commit 2b8ca3fcaf990da83df78058e11858aec4c2d34e.

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -18,7 +18,6 @@ import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
-import Reservation from "./ReservationComponent";
 import SafeAreaView from "react-native-safe-area-view";
 import { connect } from "react-redux";
 import {
@@ -82,31 +81,6 @@ const HomeNavigator = createStackNavigator(
       headerLeft: (
         <Icon
           name="home"
-          type="font-awesome"
-          iconStyle={styles.stackIcon}
-          onPress={() => navigation.toggleDrawer()}
-        />
-      ),
-    }),
-  }
-);
-
-const ReservationNavigator = createStackNavigator(
-  {
-    Reservation: { screen: Reservation },
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      headerStyle: {
-        backgroundColor: "#5637DD",
-      },
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        color: "#fff",
-      },
-      headerLeft: (
-        <Icon
-          name="tree"
           type="font-awesome"
           iconStyle={styles.stackIcon}
           onPress={() => navigation.toggleDrawer()}
@@ -201,15 +175,6 @@ const MainNavigator = createDrawerNavigator(
       navigationOptions: {
         drawerIcon: ({ tintColor }) => (
           <Icon name="list" type="font-awesome" size={24} color={tintColor} />
-        ),
-      },
-    },
-    Reservation: {
-      screen: ReservationNavigator,
-      navigationOptions: {
-        drawerLabel: "Reserve Campsite",
-        drawerIcon: ({ tintColor }) => (
-          <Icon name="tree" type="font-awesome" size={24} color={tintColor} />
         ),
       },
     },
